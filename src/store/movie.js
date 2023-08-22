eefcb11caf2fb619155bf65fee03cda7cd7ac660
박혜민 <hyeminpak@gmail.com>
2023-08-23 00:22:00 +0900

    영화 상세정보의 상태관리

--- src/store/movie.js
+++ src/store/movie.js
@@ -5,7 +5,9 @@ const store = new Store({
   page: 1,
   pageMax: 1,
   movies: [],
+  movie: {},
   loading: false,
+  message: "Search for the movie title",
 });
 
 export default store;
@@ -32,5 +34,15 @@ export const searchMivoes = async (page) => {
     console.log(error);
   } finally {
     store.state.loading = false;
+  }
+};
+export const getMovieDetails = async (id) => {
+  try {
+    const res = await fetch(
+      `https://www.omdbapi.com/?apikey=7035c60c&i=${id}&plot=full`
+    );
+    store.state.movie = await res.json();
+  } catch (error) {
+    console.log("getMovieDetails error: ", error);
   }
 };
